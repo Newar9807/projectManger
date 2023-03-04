b0cbbd4fe4f3206b71c39db49fefd0f4666f8de1
Samir Shrestha <samirshrestha9807@gmail.com>
2023-03-04 09:00:41 +0545

    Student 1st Interface Skeleton Implemented

--- php/teacherProjectDetails.php
+++ php/teacherProjectDetails.php
@@ -119,7 +119,7 @@ $host = $_SERVER['HTTP_HOST']; ?>
 
     /* Charts script */
     <script>
-        var xValues = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+        var xValues = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct","Nov","Dec"];
 
         new Chart("myChart", {
             type: "line",
@@ -129,12 +129,10 @@ $host = $_SERVER['HTTP_HOST']; ?>
                     data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478],
                     borderColor: "red",
                     fill: false
-                }, ]
+                },]
             },
             options: {
-                legend: {
-                    display: false
-                }
+                legend: { display: false }
             }
         });
     </script>
